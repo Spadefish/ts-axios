570f0fde7c03bd77dae80c34e775bb2cf29a11f3
spadefish <793378953@qq.com>
2019-11-02 18:29:38 +0800

    抽象错误处理类 增强错误信息提示

---
+++ src/axios.ts
@@ -0,0 +1,42 @@
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
+import { buildURL } from './helpers/url'
+import xhr from './xhr'
+import { transformRequest, transformResponse } from './helpers/data'
+import { processHeaders } from './helpers/headers'
+
+function axios(config: AxiosRequestConfig): AxiosPromise {
+  processConfig(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
+}
+
+// 处理传入的参数配置
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
+}
+
+// 处理url参数
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
+// 针对post请求处理body参数
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
+}
+
+export default axios
