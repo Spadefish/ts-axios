813f3ff8d1c6975dd7ad4bcebd4e844a48c583f8
spadefish <793378953@qq.com>
2019-11-04 18:54:24 +0800

    通过request函数重载实现多个参数传入

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -2,10 +2,22 @@ import { AxiosPromise, AxiosRequestConfig, Method } from '../types'
 import dispatchRequest from './dispatchRequest'
 
 export default class Axios {
-  request(config: AxiosRequestConfig): AxiosPromise {
+  // request(config: AxiosRequestConfig): AxiosPromise {
+  //   return dispatchRequest(config)
+  // }
+
+  // 通过函数重载实现传入多个参数
+  request(url: any, config?: any): AxiosPromise {
+    if (typeof url === 'string') {
+      if (!config) {
+        config = {}
+      }
+      config.url = url
+    } else {
+      config = url
+    }
     return dispatchRequest(config)
   }
-
   get(url: string, config?: AxiosRequestConfig): AxiosPromise {
     return this._requestMethodWithoutData('get', url, config)
   }
