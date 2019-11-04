eefa76cfccf35d82f2953505476c30a24fc1ae37
spadefish <793378953@qq.com>
2019-11-04 18:35:31 +0800

    通过工厂模式实现接口拓展

--- src/axios.ts
+++ src/axios.ts
@@ -1,42 +1,18 @@
-import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
-import { buildURL } from './helpers/url'
-import xhr from './xhr'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/headers'
+import { AxiosInstance } from './types'
+import Axios from './core/Axios'
+import { extend } from './helpers/util'
 
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
+function createInstance(): AxiosInstance {
+  const context = new Axios()
+  // const instance = Axios.prototype.request
+  // 方法里面有this 需要绑定到this
+  const instance = Axios.prototype.request.bind(context)
+
+  // 将实例上的方法全部导入到原型上
+  extend(instance, context)
+  return instance as AxiosInstance
 }
 
-// 处理传入的参数配置
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
-
-// 处理url参数
-function transformURL(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url, params)
-}
-
-// 针对post请求处理body参数
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
-  return res
-}
+const axios = createInstance()
 
 export default axios
